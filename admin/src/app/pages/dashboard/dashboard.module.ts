10a62a2c33720091d18238ac7303a38da230735d
Kent Sumayang <kentjames2002@gmail.com>
2023-10-23 09:08:43 -0700

    push back-up cashiering

---
+++ admin/src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {
+  AvatarModule,
+  ButtonGroupModule,
+  ButtonModule,
+  CardModule,
+  FormModule,
+  GridModule,
+  NavModule,
+  ProgressModule,
+  TableModule,
+  TabsModule
+} from '@coreui/angular';
+import { IconModule } from '@coreui/icons-angular';
+import { ChartjsModule } from '@coreui/angular-chartjs';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+
+import { WidgetsModule } from '../widgets/widgets.module';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    DashboardRoutingModule,
+    CardModule,
+    NavModule,
+    IconModule,
+    TabsModule,
+    CommonModule,
+    GridModule,
+    ProgressModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    FormModule,
+    ButtonModule,
+    ButtonGroupModule,
+    ChartjsModule,
+    AvatarModule,
+    TableModule,
+    WidgetsModule
+  ],
+ 
+})
+export class DashboardModule {
+}
