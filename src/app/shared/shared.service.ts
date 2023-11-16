fc967aaacc53e84a5724d01e8ebf5da730e77676
Aron Balais <112226479+Tinga-r0n@users.noreply.github.com>
2023-11-16 14:49:38 +0800

    added select/unselect functionality

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -1,4 +1,3 @@
-// shared.service.ts
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -6,17 +5,28 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class SharedService {
-    private selectedCustomerObject: any;
+  private selectedCustomerObject: any;
   private selectedCustomerSource = new BehaviorSubject<string>(''); 
   selectedCustomer$ = this.selectedCustomerSource.asObservable();
 
+  private selectedCustomerKey = 'selectedCustomer';
+
+  constructor() {
+    const selectedCustomer = localStorage.getItem(this.selectedCustomerKey);
+    if (selectedCustomer) {
+      this.selectedCustomerSource.next(selectedCustomer);
+    }
+  }
+
   setSelectedCustomer(customerName: string): void {
+    localStorage.setItem(this.selectedCustomerKey, customerName);
     this.selectedCustomerSource.next(customerName);
   }
+
   setSelectedCustomerObject(customer: any): void {
     this.selectedCustomerObject = customer;
   }
-  
+
   getSelectedCustomerObject(): any {
     return this.selectedCustomerObject;
   }
