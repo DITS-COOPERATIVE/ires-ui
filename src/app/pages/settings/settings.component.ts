51dfd3575689718af413e65061eb6240125da886
Aron Balais <112226479+Tinga-r0n@users.noreply.github.com>
2023-12-04 16:37:44 +0800

    added cashier

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CashierService } from 'src/app/services/settings/cashier.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
+  constructor(  private cashiersService: CashierService){
+  }
+  full_name!: string;
+  gender!: string;
+  mobile_no!: string;
+  address!: string;
+  image!: string;
+  errors: any = {};
+  uploadedImage: any;
+  selectedImage: string = '';
 
+
+
+  saveCashier() {
+    var inputData = {
+      full_name: this.full_name,
+      gender: this.gender,
+      mobile_no: this.mobile_no,
+      address: this.address,
+      image: this.image,
+    };
+
+    this.cashiersService.saveCustomer(inputData).subscribe({
+      next: (res:any) => {
+        this.full_name ='' ;
+        this.gender = '';
+        this.address = '';
+        this.image = '';
+        this.selectedImage = '';
+      },
+      error: (err: any) => {
+        this.errors = err.error.errors;
+      },
+    });
+  }
 }
