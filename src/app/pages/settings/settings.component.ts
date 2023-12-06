ae9525167165dec81d8251789a46239523ae159c
Aron Balais <112226479+Tinga-r0n@users.noreply.github.com>
2023-12-06 12:25:44 +0800

    setting

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -12,8 +12,10 @@ export class SettingsComponent {
   full_name!: string;
   gender!: string;
   mobile_no!: string;
+  email!: string;
   address!: string;
   image!: string;
+  password!: string;
   errors: any = {};
   uploadedImage: any;
   selectedImage: string = '';
@@ -23,19 +25,23 @@ export class SettingsComponent {
   saveCashier() {
     var inputData = {
       full_name: this.full_name,
+      email:this.email,
       gender: this.gender,
       mobile_no: this.mobile_no,
       address: this.address,
+      password:this.password,
       image: this.image,
     };
 
     this.cashiersService.saveCustomer(inputData).subscribe({
       next: (res:any) => {
         this.full_name ='' ;
+        this.email;
         this.gender = '';
         this.address = '';
         this.image = '';
         this.selectedImage = '';
+        this.password = '';
       },
       error: (err: any) => {
         this.errors = err.error.errors;
