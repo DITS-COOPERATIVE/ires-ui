523ec966a352af3162bdbc0687903ee591cf35ab
Aron Balais <112226479+Tinga-r0n@users.noreply.github.com>
2023-12-07 10:02:53 +0800

    print functionality

--- src/app/pages/transactions/payment/payment.component.ts
+++ src/app/pages/transactions/payment/payment.component.ts
@@ -34,6 +34,7 @@ export class PaymentComponent {
   model!: string;
   price!: string;
   quantity!: string;
+  discount!:number;
   payment: boolean = false;
   receiptContent: string = '';
 
@@ -76,7 +77,189 @@ export class PaymentComponent {
     this.paymentSuccess = true;
   }
 
-  onPrintReceipt(): void {
-    window.print();
+
+  printReceipt() {
+    const printWindow = window.open('', '', 'width=800,height=600');
+    if (printWindow) {
+      printWindow.document.open();
+      printWindow.document.write(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+          <title>Receipt</title>
+            <style>
+            .receipt_header {
+              padding-bottom: 10px;
+              border-bottom: 1px dashed #000;
+              text-align: center;
+            }
+            
+            .receipt_header h1 {
+              font-size: 20px;
+              margin-bottom: 5px;
+              text-transform: uppercase;
+            }
+            
+            .receipt_header h1 span {
+              display: block;
+              font-size: 25px;
+            }
+            
+            .receipt_header h2 {
+              font-size: 14px;
+              color: #727070;
+              font-weight: 300;
+            }
+            
+            .receipt_header h2 span {
+              display: block;
+            }
+            
+            .receipt_body {
+              margin-top: 5px;
+            }
+            
+            table {
+              width: 100%;
+            }
+
+            thead, tfoot {
+              position: relative;
+            }
+
+            thead th:not(:last-child) {
+              text-align: left;
+            }
+            thead::after {
+              content: '';
+              width: 100%;
+              display: block;
+              position: absolute;
+            }
+
+            tbody td:not(:last-child), tfoot td:not(:last-child) {
+              text-align: left;
+            }
+
+
+            tbody tr:first-child td {
+              padding-top: 15px;
+            }
+
+            tbody tr:last-child td {
+              padding-bottom: 15px;
+            }
+
+            tfoot tr:first-child td {
+              padding-top: 15px;
+            }
+
+            tfoot::before {
+              content: '';
+              width: 100%;
+              border-top: 1px dashed #000;
+              display: block;
+              position: absolute;
+            }
+
+            tfoot tr:first-child td:first-child, tfoot tr:first-child td:last-child {
+              font-weight: bold;
+              font-size: 20px;
+            }
+
+            .date_time_con {
+              display: flex;
+              justify-content: end;
+              column-gap: 25px;
+            }
+
+            .items {
+              margin-top: 25px;
+            }
+
+            h3 {
+              border-top: 1px dashed #000;
+              padding-top: 10px;
+              margin-top: 25px;
+              text-align: center;
+              text-transform: uppercase;
+            }
+            </style>
+          </head>
+          <body>
+            <div class="container px-3" style="height: 500px; overflow: auto;">
+              <div class="receipt_header">
+              <h1><img src="assets/dits.png" alt="" width="10%" class="mt-3"> <span>DITS</span></h1>
+                <h2>365 Triumfo St, Carigara, Philippines, 6530
+                  <span>Tel: +1 012 345 67 89</span>
+                </h2>
+              </div>
+      
+              <div class="receipt_body">
+                <div class="date_time_con">
+                  <div class="date">11/12/2020</div>
+                  <div class="time">11:13:06 AM</div>
+                </div>
+      
+                <div class="items">
+                  <table class="text-start">
+                    <thead>
+                      <tr>
+                        <th>QUANTITY</th>
+                        <th>ITEM</th>
+                        <th>PRICE</th>
+                        <th>DISCOUNT</th>
+                      </tr>
+                    </thead>
+                
+                    <tbody>
+                      ${this.cart.map(cartItem => `
+                        <tr>
+                          <td>${cartItem.quantity}</td>
+                          <td>${cartItem.name}</td>
+                          <td>₱${cartItem.price}</td>
+                          <td>${cartItem.discount > 0 ? cartItem.discount : ''}</td>
+                        </tr>
+                      `).join('')}
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <td colspan="3">Total</td>
+                        <td>₱${this.calculateTotal()}</td>
+                      </tr>
+                
+                      <tr>
+                        <td colspan="3">Cash</td>
+                        <td>₱${this.amountPaid}</td>
+                      </tr>
+                
+                      <tr>
+                        <td colspan="3">Change</td>
+                        <td>₱${this.calculateChange()}</td>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </div>
+                <h3>Thank You!</h3>
+              </div>
+            </div>
+      
+            <script type="text/javascript">
+              setTimeout(function() {
+                const receiptContent = document.querySelector('.container')?.innerHTML;
+                if (receiptContent) {
+                  document.body.innerHTML = receiptContent;
+                  window.print();
+                  window.onafterprint = function() {
+                    window.close();
+                  };
+                }
+              }, 100);
+            </script>
+          </body>
+        </html>
+      `);
+      printWindow.document.close();
+    }
   }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,6 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CustomersResponse, CustomersService } from '../../services/customers/customers.service';
+import { DatePipe } from '@angular/common';
+import JsBarcode from 'jsbarcode';
 
 @Component({
   selector: 'app-modal',
@@ -8,10 +10,12 @@ import { CustomersResponse, CustomersService } from '../../services/customers/cu
   styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent {
+  @ViewChild('barcode') barcode!: ElementRef<any>;
   constructor(
     private customersService: CustomersService,
     public dialogRef: MatDialogRef<ModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private datePipe: DatePipe,
   ) {this.value = this.data.barcode;}
   customers!: CustomersResponse[];
   full_name!: string;
@@ -21,8 +25,51 @@ export class ModalComponent {
     this.dialogRef.close();
   }
 
-  onPrint(): void {
-    // Add your print logic here
-    console.log('Printing...');
+  generateBarcodeImage(barcodeValue: string): string {
+    const canvas = document.createElement('canvas');
+    JsBarcode(canvas, barcodeValue, { format: 'CODE128' }); // Use the desired format
+  
+    const base64Image = canvas.toDataURL('image/png');
+    return base64Image;
   }
+
+  onPrint(): void {
+    const formattedDate = this.datePipe.transform(this.data.createdDate, 'medium');
+    const barcodeImage = this.generateBarcodeImage(this.data.barcode);
+  
+    const printWindow = window.open('', '', 'width=800,height=600');
+    if (printWindow) {
+      printWindow.document.open();
+      printWindow.document.write(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Registration Details</title>
+            <style>
+              /* Add any custom styles for the printout here */
+            </style>
+          </head>
+          <body>
+            <h2 class="modal-title">Registration Details</h2>
+            <div>
+              <p>Name: ${this.data.full_name}</p>
+              <img src="${barcodeImage}" alt="Barcode Image" />
+              <p>Date Created: ${formattedDate}</p>
+            </div>
+  
+            <script type="text/javascript">
+              setTimeout(function() {
+                window.print();
+                window.onafterprint = function() {
+                  window.close();
+                };
+              }, 100);
+            </script>
+          </body>
+        </html>
+      `);
+      printWindow.document.close();
+    }
+  }
+  
 }
